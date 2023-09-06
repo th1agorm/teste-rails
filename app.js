605417b04a6a636d965500da0cb245c5e73c6fca
Thiago R <southiagorm@gmail.com>
2023-09-06 10:28:00 -0300

    icomming webhook 7

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/",(request, response, next) =>{
-    response.send("Hello World 6");
+    response.send("Hello World 7");
 });
 
 app.listen(process.env.PORT, ()=>{
