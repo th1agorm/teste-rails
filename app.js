f9f728460f8586b56d42bb12c35ec8c85f8f0239
Thiago R <southiagorm@gmail.com>
2023-09-13 13:24:48 -0300

    ajuste .env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/",(request, response, next) =>{
-    response.send("Hello World 29");
+    response.send(`Hello World 29: ${process.env.RANDOM_DATA}`);
 })
 
 app.listen(process.env.PORT, ()=>{
